193b2379b6aa8718c1dfb4305e8539eec25bf2e3
Rafael Zavala <90066300+Rafaelowsky@users.noreply.github.com>
2024-08-14 20:08:10 +0000

    Borre archivo py

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   //std::cout<<"Hello World";
+   
+   cout<<"Hola mundo; me quebre la cabeza imprimiendo este Hola mundo";
+
+    return 0;
+}
