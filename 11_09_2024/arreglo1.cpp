70c6974f695c7c7725f828714a7e630fee2d7062
Rafaelowsky <319055539@cuautitlan.unam.mx>
2024-09-11 20:19:18 -0600

    Elimine el TODO y tambien comente codigo

--- 11_09_2024/arreglo1.cpp
+++ 11_09_2024/arreglo1.cpp
@@ -4,23 +4,32 @@
 	11 - Septiembre -2024
 */
 
-// TODO: Ciclo while para poder iterar sobre listas
 #include <iostream>
 #include <cstdlib>
 
 using namespace std;
 
 int main() {
-int A[][4] = {{1,2,3,4},{5,6,7,8}};
+    int A[][4] = {{1,2,3,4},{5,6,7,8}};
+    int filas = 0;
+    int columnas = 0;
 
-	system("color E9"); // Cambia el color del Shell
+        system("color E9"); // Cambia el color del Shell
 
-   for(int fila = 0; fila < 2; fila ++) { // Esta primera sentencia itera para las filas
-     for(int columna = 0; columna < 4; columna ++) // Esta sentencia itera para las columnas
-     cout << A [fila] [columna] << " ";
-     cout << endl;
-   }
+       for(int fila = 0; fila < 2; fila ++) { // Esta primera sentencia itera para las filas
+         for(int columna = 0; columna < 4; columna ++) // Esta sentencia itera para las columnas
+         cout << A [fila] [columna] << " ";
+         cout << endl;
+       }
 
-   return 0;
-
+        /* No se puede hacer con while porque size no es para matrices
+            es un método que es para los datos tipo int
+       while ( filas < A.size()) {
+            columnas = 0;
+            while (columnas <  A[filas].size()) {
+                cout << "Elemento en [ " << filas << "][" << columnas << "]: " << A[filas][columnas] << endl;
+            }
+       }
+       */
+       return 0;
 }
